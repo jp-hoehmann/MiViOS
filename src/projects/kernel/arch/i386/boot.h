e0aca90e3076ac2a676a41bb1e50f5cfea25e040
NuvandaPV <jp@hhmn.de>
2018-08-29 16:29:16 +0200

    CAPS for constants, snake for pointers, dammit.

---
+++ src/projects/kernel/arch/i386/boot.h
@@ -0,0 +1,27 @@
+/*
+ * boot.h
+ *
+ * Created by Jean-Pierre Höhmann on 29.08.18.
+ *
+ * Copyright 2018 Jean-Pierre Höhmann (@NuvandaPV) <jean-pierre@höhmann.info>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ARCH_I386_BOOT_H
+#define ARCH_I386_BOOT_H 1
+
+extern void _kernel_end;
+
+#endif // ARCH_I386_BOOT_H
+
