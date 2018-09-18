ff11551dfa9ba5e93b5d143f18f0d397e9ee4502
NuvandaPV <jp@hhmn.de>
2018-09-18 14:54:26 +0200

    Add a userspace application

---
+++ src/projects/example/example/example.h
@@ -0,0 +1,28 @@
+/*
+ * example.h
+ *
+ * Created by Jean-Pierre Höhmann on 2018-09-16.
+ *
+ * Copyright 2018 Jean-Pierre Höhmann (@NuvandaPV) <jean-pierre@höhmann.info>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HELLO_HELLO_H
+#define HELLO_HELLO_H
+
+#include <stddef.h>
+
+int main(size_t argc, char* argv[]);
+
+#endif // HELLO_HELLO_H
