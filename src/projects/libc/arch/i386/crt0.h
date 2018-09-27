0f271a02e2ff81e3cabf70f99d9e5e032b52feaa
NuvandaPV <jp@hhmn.de>
2018-09-27 19:00:00 +0200

    Add crt0.h

---
+++ src/projects/libc/arch/i386/crt0.h
@@ -0,0 +1,32 @@
+/*
+ * crt0.h
+ *
+ * Created by Jean-Pierre Höhmann on 2018-09-27.
+ *
+ * Copyright 2018 Jean-Pierre Höhmann (@NuvandaPV) <jean-pierre@höhmann.info>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ARCH_I386_CRT0_H
+#define ARCH_I386_CRT0_H
+
+// Stub.
+
+#ifndef __assembler
+
+// Stub.
+
+#endif // ! __assembler
+
+#endif // ! ARCH_I386_CRT0_H
